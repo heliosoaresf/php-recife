fc88045c08b6f8d9e4b60f7a645381aa2c25f10e
heliosoaresf <heliosoares_ti@yahoo.com.br>
2018-05-15 15:22:26 -0300

    versão teste

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo "<h1>Openshift Workshop v3.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
-$conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
+$conn = new mysqli("127.0.0.1", "redhat", "redhat@123", "workshop");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
